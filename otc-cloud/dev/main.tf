68ad57ed185c950c76478d4a0a7a3e470dbe1dcb
Victor Getz <sd>
2023-09-18 09:49:02 +0200

    feat: bump project factory version

--- otc-cloud/dev/main.tf
+++ otc-cloud/dev/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source             = "registry.terraform.io/iits-consulting/project-factory/opentelekomcloud//modules/vpc"
-  version            = "5.3.0"
+  version            = "5.5.0"
   name               = "${var.context}-${var.stage}-vpc"
   cidr_block         = var.vpc_cidr
   enable_shared_snat = false
@@ -9,7 +9,7 @@ module "vpc" {
 
 module "snat" {
   source      = "registry.terraform.io/iits-consulting/project-factory/opentelekomcloud//modules/snat"
-  version     = "5.3.0"
+  version     = "5.5.0"
   name_prefix = "${var.context}-${var.stage}"
   subnet_id   = module.vpc.subnets["kubernetes-subnet"].id
   vpc_id      = module.vpc.vpc.id
@@ -18,7 +18,7 @@ module "snat" {
 
 module "cce" {
   source  = "registry.terraform.io/iits-consulting/project-factory/opentelekomcloud//modules/cce"
-  version = "5.3.0"
+  version = "5.5.0"
 
   name                           = "${var.context}-${var.stage}"
   cluster_vpc_id                 = module.vpc.vpc.id
@@ -45,7 +45,7 @@ module "cce" {
 
 module "loadbalancer" {
   source       = "registry.terraform.io/iits-consulting/project-factory/opentelekomcloud//modules/loadbalancer"
-  version      = "5.3.0"
+  version      = "5.5.0"
   context_name = var.context
   subnet_id    = module.vpc.subnets["kubernetes-subnet"].subnet_id
   stage_name   = var.stage
@@ -54,7 +54,7 @@ module "loadbalancer" {
 
 module "private_dns" {
   source  = "registry.terraform.io/iits-consulting/project-factory/opentelekomcloud//modules/private_dns"
-  version = "5.3.0"
+  version = "5.5.0"
   domain  = "vpc.private"
   a_records = {
     kubernetes = [split(":", trimprefix(module.cce.cluster_private_ip, "https://"))[0]]
@@ -64,7 +64,7 @@ module "private_dns" {
 
 module "public_dns" {
   source  = "registry.terraform.io/iits-consulting/project-factory/opentelekomcloud//modules/public_dns"
-  version = "5.3.0"
+  version = "5.5.0"
   domain  = var.domain_name
   email   = var.email
   a_records = {
@@ -76,7 +76,7 @@ module "public_dns" {
 module "encyrpted_secrets_bucket" {
   providers         = { opentelekomcloud = opentelekomcloud.top_level_project }
   source            = "registry.terraform.io/iits-consulting/project-factory/opentelekomcloud//modules/obs_secrets_writer"
-  version           = "5.3.0"
+  version           = "5.5.0"
   bucket_name       = replace(lower("${var.region}-${var.context}-${var.stage}-stage-secrets"), "_", "-")
   bucket_object_key = "terraform-secrets"
   secrets = {
